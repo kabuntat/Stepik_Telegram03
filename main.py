422bd90fc4268c17b2dd5a269ed8c6dab8737620
Ilya Gaynullin <igaynullin@gmail.com>
2025-11-04 16:37:50 +0300

    from home

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from config_data.config import Config, load_config
 from handlers import other, user
+from aiogram.types import BotCommand
+
 
 # Инициализируем логгер
 logger = logging.getLogger(__name__)
+
+
+# Создаем асинхронную функцию
+async def set_main_menu(bot: Bot):
+
+    # Создаем список с командами и их описанием для кнопки menu
+    main_menu_commands = [
+        BotCommand(command='/start',
+                   description='Запустить бота'),
+        BotCommand(command='/help',
+                   description='Справка по работе бота')
+    ]
+
+    await bot.set_my_commands(main_menu_commands)
 
 # Функция конфигурирования и запуска бота
 async def main() -> None:
@@ -22,12 +40,16 @@ async def main() -> None:
     logger.info("Starting bot")
 
     # Инициализируем бот и диспетчер
-    bot=Bot(token=config.bot.token)
+    bot=Bot(token=config.bot.token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
 
     # Регистриуем роутеры в диспетчере
     dp.include_router(user.router_user)
     dp.include_router(other.router_other)
+
+    # Регистрируем асинхронную функцию в диспетчере,
+    # которая будет выполняться на старте бота,
+    dp.startup.register(set_main_menu)
 
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
